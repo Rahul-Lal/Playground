3ea527662ecc7db4b7c6904ccd2774f536d8bb0a
Rahul21Lal <Rahul21Lal@gmail.com>
2016-07-20 12:08:45 +1200

    Numerical Function
    number buttons can now add input to textbox.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            txtOutput.Text = "";
+        }
+
+        public int firstvalue;
+        public int secondvalue;
+        public int thirdvalue;
+        public int finalanswer;
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "1";
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "2";
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "3";
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "4";
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "5";
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "6";
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "7";
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "8";
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "9";
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "0";
+        }
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = "";
+        }
+
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = "";
+            firstvalue -= firstvalue;
+            secondvalue -= secondvalue;
+        }
+    }
+}
